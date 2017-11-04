033e1c930fb84fb83ff7c9e1522cd6c204f88513
AAM77 <adeel.a.mhd@gmail.com>
2017-11-04 11:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 }
+else if (name === "Joe") {
+  console.log("This name is more correct")
+
+}
 
 // Don't worry about this
 module.exports = { name, height
 }
-
